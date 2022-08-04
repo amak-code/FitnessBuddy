b93b09c39dce4668ffe9ac0dc59511ebbb086c39
Aleksandra Makhrova <amakhro@gmail.com>
2022-08-04 14:00:01 -0700

    test after deleting cocoapods

---
+++ FitnessBuddy/FitnessBuddy/Views/SignInViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  SignInViewModel.swift
+//  FitnessBuddy
+//
+//  Created by antikiller on 04.08.2022.
+//
+
+import SwiftUI
+import FirebaseAuth
+import FirebaseDatabase
+
+//class SignInViewMOdel: ObservableObject {
+//    
+//    var entries: [[String : Dictionary<String, String>]]
+//    init() {
+//        entries = []
+//    }
+//    
+//    let auth = Auth.auth()
+//    
+//    var isSighnedIn: Bool {
+//        return auth.currentUser != nil
+//    }
+//    
+//    func signIn(email: String, password: String){
+//        auth.signIn(withEmail: email, password: password){
+//        
+//        
+//    }
+//    
+//    func signUp(email: String, password: String){
+//        
+//    }
+//}
