941ff8f334da796f27ab712ba65dcac10f279385
Lily Tran <tran.lily8@gmail.com>
2022-08-04 16:41:54 -0700

    push to test main branch

--- FitnessBuddy/FitnessBuddy/Views/Exercise/ExerciseDBController.swift
+++ FitnessBuddy/FitnessBuddy/Views/Exercise/ExerciseDBController.swift
@@ -9,48 +9,45 @@ import Foundation
 
 class ExerciseDBController {
     static let baseURL = URL(string: "https://exercisedb.p.rapidapi.com")
-    static let searchbodyPartComponent = "/exercises/bodyPart"
+    static let searchbodyPartComponent = "/exercises"
     
-    static func fetchExercises(by part: bodyParts, completion: @escaping (Result<[Exercise], NetworkError>) -> Void) {
+    static func fetchExercises( completion: @escaping (Result<[Exercise], NetworkError>) -> Void ) {
         let headers = [
             "X-RapidAPI-Key": "6b1fb347bfmshfc36be9e526e3b9p1c7b99jsn7a1de3800113",
             "X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
         ]
         
-        
         guard let baseURL = baseURL else {
-            return
+            return completion(.failure(.invalidURL))
         }
         
-        var url = baseURL.appendingPathComponent(searchbodyPartComponent)
-        url = url.appendingPathComponent(part.rawValue)
+        let url = baseURL.appendingPathComponent(searchbodyPartComponent)
+        // url = url.appendingPathComponent(part.rawValue)
         
         var request = URLRequest(url: url)
-//        print(request)
-//        print(url)
         request.httpMethod = "GET"
         request.allHTTPHeaderFields = headers
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
         URLSession.shared.dataTask(with: request as URLRequest) { data, _, error in
             if let error = error {
-                print("There is an error")
-                return
+                return completion(.failure(.thrownError(error)))
             }
             
             guard let data = data else {
-                print("invalid data")
-                return }
+                return completion(.failure(.noData))
+            }
             do {
+                // In case you want to see the JSON. cannot just use this because JSONSerialization returns an AnyObject array.
                 //let json = try JSONSerialization.jsonObject(with: data, options: [])
                 //print(json)
                 let exercises = try JSONDecoder().decode([Exercise].self, from: data)
-                completion(.success(exercises))
-                return
+                return completion(.success(exercises))
+                
                 
             } catch {
                 print("Error in the do catch")
-                return
+                return completion(.failure(.unableToDecode))
             }
         }.resume()
     }
