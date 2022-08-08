aac62eb21cc547f93099cff8f2076efa86fc2154
Aleksandra Makhrova <amakhro@gmail.com>
2022-08-07 22:18:50 -0700

    AuthViews are connected

--- FitnessBuddy/FitnessBuddy/Views/UserAuthViews/SignupView.swift
+++ FitnessBuddy/FitnessBuddy/Views/UserAuthViews/SignupView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct SignupView: View {
     
     @ObservedObject var signInVM = SignInViewModel()
-    
+    @State var showMainView = false
     @State var emailText: String
     @State var usernameText: String
     @State var passwordText: String
@@ -19,18 +19,36 @@ struct SignupView: View {
             VStack(alignment: .center, spacing: 20) {
                 
                 FormField(label: "Username", placeholder: "Enter your username", input: $usernameText)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
                 
                 FormField(label: "E-mail", placeholder: "Enter your e-mail", input: $emailText)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
                 
                 FormField(label: "Password", placeholder: "Create a password", input: $passwordText)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
                 
                 Spacer()
-                Button {
-                    SignInViewModel().signUpUser(email: emailText, password: passwordText, name: usernameText)
-                } label: {
-                    ButtonLabel(text: "Sign Up", colorName: "AccentLight", textColor: "LightText")
-                }
                 
+                NavigationLink(destination: MenuView(), isActive: $showMainView){
+                    
+                    Button {
+                        guard !emailText.isEmpty, !passwordText.isEmpty, !usernameText.isEmpty else {
+                            return}
+                        signInVM.signUpUser(email: emailText, password: passwordText, name: usernameText)
+                        emailText = ""
+                        passwordText = ""
+                        usernameText = ""
+                        self.showMainView = true
+
+                    } label: {
+                        ButtonLabel(text: "Sign Up", colorName: "AccentLight", textColor: "LightText")
+                    }
+                    
+                }
+              
                 Text("--   Or   --")
                 
                 NavigationLink {
