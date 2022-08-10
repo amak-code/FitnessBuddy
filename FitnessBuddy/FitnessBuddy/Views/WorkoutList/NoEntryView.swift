a2f50f5eaaf01a7d7f2033341a181d0fc4a7c8ae
tanya_bond <bondarenkotatiana96@gmail.com>
2022-08-10 16:11:35 -0700

    Fix close popup button and navigation issue

--- FitnessBuddy/FitnessBuddy/Views/WorkoutList/NoEntryView.swift
+++ FitnessBuddy/FitnessBuddy/Views/WorkoutList/NoEntryView.swift
@@ -13,38 +13,47 @@ struct NoEntryView: View {
     @Binding var showingPopup: Bool
 
     var body: some View {
-        ScrollView {
-            VStack(spacing: 20) {
-                Text("You haven't created any workouts yet. Add your favorite workouts!")
-                    .padding(.bottom, 20)
+        ZStack {
+            if showingPopup {
+                AddWorkoutSplitView(listViewModel: WorkoutListViewModel(), showingPopup: $showingPopup)
+            } else {
+                ScrollView {
+                    VStack(spacing: 20) {
+                        Text("You haven't created any workouts yet. Add your favorite workouts!")
+                            .padding(.bottom, 20)
 
-                NavigationLink(
-                    destination: AddWorkoutSplitView( listViewModel: WorkoutListViewModel(), showingPopup: $showingPopup),
-                    label: {
-                        Text("Add")
-                            .foregroundColor(.white)
-                            .font(.headline)
-                            .frame(height: 55)
-                            .frame(maxWidth: .infinity)
-                            .background(animate ? AccentColor : .green)
-                            .cornerRadius(10)
-                    })
-                    .padding(.horizontal, animate ? 30 : 50)
-                    .shadow(
-                        color: animate ? AccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
-                        radius: animate ? 30 : 10,
-                        x: 0,
-                        y: animate ? 50 : 30)
-                    .scaleEffect(animate ? 1.1 : 1.0)
-                    .offset(y: animate ? -7 : 0)
+                        Button {
+                            showingPopup = true
+                        }
+                            label: {
+                                Text("Add")
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                                    .frame(height: 55)
+                                    .frame(maxWidth: .infinity)
+                                    .background(animate ? AccentColor : .green)
+                                    .cornerRadius(10)
+                            }
+                            .padding(.horizontal, animate ? 30 : 50)
+                            .shadow(
+                                color: animate ? AccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                                radius: animate ? 30 : 10,
+                                x: 0,
+                                y: animate ? 50 : 30)
+                            .scaleEffect(animate ? 1.1 : 1.0)
+                            .offset(y: animate ? -7 : 0)
+                    }
+                    .frame(maxWidth: 300)
+                    .multilineTextAlignment(.center)
+                    .padding(40)
+                }
+                .onAppear {
+                    addAnimation()
+                }
             }
-            .frame(maxWidth: 300)
-            .multilineTextAlignment(.center)
-            .padding(40)
-            .onAppear(perform: addAnimation)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+            
 
     func addAnimation() {
         guard !animate else { return }
