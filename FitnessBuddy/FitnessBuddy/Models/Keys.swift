11fdaf86b551c930f0c65b1b796562916a71851e
Lily Tran <lily@Lilys-MBP.lan>
2022-08-12 14:03:40 -0700

    enum for API keys, future to do: make keys hidden

---
+++ FitnessBuddy/FitnessBuddy/Models/Keys.swift
@@ -0,0 +1,12 @@
+//
+//  Keys.swift
+//  FitnessBuddy
+//
+//  Created by Lily Tran on 8/12/22.
+//
+
+import Foundation
+
+enum Keys: String {
+    case exerciseDB = "98460464a2mshe4698707b991021p18ed31jsn331d5889e7cc"
+}
